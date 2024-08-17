d2d2f18b8e326c87e23aa8b160ebdf7885f37646
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2024-08-17 17:02:52 -0400

    Platforms and Portals now enable/disable their respective player controllers

--- Assets/Scripts/Player/Player Scale Portal.cs
+++ Assets/Scripts/Player/Player Scale Portal.cs
@@ -7,12 +7,33 @@ using UnityEngine;
 
 public class PlayerScalePortal : MonoBehaviour
 {
+    #region Inspector Variables
+    [SerializeField] Transform teleportPoint;
+    #endregion
+
+    #region Hidden Variables
+    private GameManager gameManager;
+    #endregion
+
+    #region Functions
+    private void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+    #endregion
+
     #region Collider Functions
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && other.GetComponent<FPSController>().playerScale == FPSController.PlayerScale.NORMAL)
         {
-            // Change the player scale to small and teleport to last used scale platform
+            // Disable the normal player and enable the small player
+            gameManager.normalPlayer.SetActive(false);
+            gameManager.smallPlayer.SetActive(true);
+
+            // Move the normal player back to their teleport point
+            gameManager.normalPlayer.transform.position = teleportPoint.position;
+
             Debug.Log("Player has entered the portal");
         }
     }
