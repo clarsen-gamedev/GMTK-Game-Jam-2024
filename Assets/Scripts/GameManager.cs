55af9a16dc478b7029df0a3452b65adda9ae05ce
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2024-08-17 18:21:25 -0400

    BROOM

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@
 // Date: 08/17/2024
 // Description: 
 
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -11,17 +12,24 @@ public class GameManager : MonoBehaviour
     [Header("Prefabs")]
     public GameObject normalPlayer;
     public GameObject smallPlayer;
+
+    [Header("UI Elements")]
+    public TMP_Text UI_CollectibleCounter;
     #endregion
 
     #region Hidden Variables
+    [HideInInspector] public int collectiblesFound;
     #endregion
 
     #region Functions
     private void Awake()
     {
         // Start game with small player active and normal player inactive
-        normalPlayer.SetActive(true);
-        smallPlayer.SetActive(false);
+        normalPlayer.SetActive(false);
+        smallPlayer.SetActive(true);
+
+        // Set collectiblesFound to 0
+        collectiblesFound = 0;
     }
     #endregion
 }
