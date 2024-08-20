8c9e9b45ad6f64e127cce357bcf658059c0b25ef
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2024-08-20 02:51:40 -0400

    Fixed animations

--- Assets/Scripts/Objects/Collectible.cs
+++ Assets/Scripts/Objects/Collectible.cs
@@ -34,7 +34,6 @@ public class Collectible : MonoBehaviour
             gameManager.CheckWinCondition();
 
             // Delete collectible
-            gameObject.GetComponent<Animator>().enabled = false;
             Destroy(gameObject);
         }
     }
